da8476ac16e2f81ec2e5663b3a5bb2b627ca5e8e
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2025-07-31 21:36:03 +0530

    Update main.tf

--- infra/live/dev/main.tf
+++ infra/live/dev/main.tf
@@ -1,5 +1,15 @@
-provider "aws" {
-  region = var.region
+#provider "aws" {
+#  region = var.region
+#}
+terraform {
+  required_version = ">= 1.6.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 data "aws_vpc" "default" {
